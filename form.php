1afac04fc91d854cb6852393ed28b2fe42862247
shahzaibriaz69 <shahzaibriaz0023@gmail.com>
2025-11-26 11:21:06 +0500

    curd form

---
+++ form.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curd</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="title">
+            Register Form
+        </div>
+
+        <div class="form">
+            <div class="input-field">
+                <label>First Name</label>
+                <input type="text" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Last Name</label>
+                <input type="text" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Password</label>
+                <input type="password" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Confirm password</label>
+                <input type="password" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Gender</label>
+                <input type="text" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Email</label>
+                <input type="text" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Phone Number</label>
+                <input type="text" class="input">
+            </div>
+
+            <div class="input-field">
+                <label>Address</label>
+                <input type="text" class="input">
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
